98d89f1eb8428864e26160bf2516cc713fece5fb
KaweeshaD20 <Kaweeshadewranga05@gmail.com>
2026-04-22 00:22:24 +0530

    feat(reviews): update reviewRoutes for new review endpoints

--- server/routes/reviewRoutes.js
+++ server/routes/reviewRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { protect, requireBuyer } = require('../middleware/auth');
-const { getReviewsForSeller, createReview, updateReview, deleteReview } = require('../controllers/reviewController');
+const { getReviewsForSeller, createReview, updateReview, deleteReview, getMyReviews } = require('../controllers/reviewController');
 
 const router = express.Router();
 
+router.get('/me', protect, requireBuyer, getMyReviews);
 router.get('/seller/:sellerId', getReviewsForSeller);
 router.post('/', protect, requireBuyer, createReview);
 router.patch('/:reviewId', protect, requireBuyer, updateReview);
